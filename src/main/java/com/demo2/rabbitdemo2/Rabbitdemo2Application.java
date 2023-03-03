eac47288c5bc61d88baa07c8f09db7486862caef
Valentina Santa Muñoz <valentinas792 @gmail.com>
2023-03-03 17:51:59 -0500

    Proyecto RabbitMQ y Spring Boot
    Se sube proyecto de RabbitMQ con Spring Boot

---
+++ src/main/java/com/demo2/rabbitdemo2/Rabbitdemo2Application.java
@@ -0,0 +1,13 @@
+package com.demo2.rabbitdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rabbitdemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rabbitdemo2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/demo2/rabbitdemo2/dto/User.java
@@ -0,0 +1,14 @@
+package com.demo2.rabbitdemo2.dto;
+
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+}
